d48213309cbd2c42b99d47ff3004c55d3a2505ef
Daiyaz <uchihachikk3@gmail.com>
2024-12-13 15:50:06 +0700

    Done_lab9_2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 //Write the function printO() here
 
+void printO(int N,int M){
+	if(M<=0||N<=0){
+		cout << "Invalid input";
+	}
+	int i=0,j=0;
+	for(i=0;i<N;i++){
+		for(j=0;j<M;j++){
+			cout<<"O";
+		}
+		cout<<endl;
+	}
+}
+
 int main(){
 	
 	printO(2,2);
